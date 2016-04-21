cba426ff21914cb180746d77285e47c88b7b865c
mirela-abh <mirela@atlantbh.com>
2016-04-21 13:54:43 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentServices::Application.config.secret_token = 'b32ca29fc3c5ba4f9889e3d4187dc32895af28a1f478aa254ef52a508a71593e225a20205877a9208a56e1cc47d0fe0d5825afa9aa11ba51e09ce1db19df11a3'
